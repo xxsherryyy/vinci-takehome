4a9b5f602c892fa07df4058a290876a33af66406
xxsherryyy <sherryxxyang@gmail.com>
2025-10-06 02:05:30 -0500

    copy change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container">
         <h1>Vinci Takehome - Angry Shiba</h1>
-        <h2>Pull on the dog cheeks to make it angry</h2>
+        <h2>Pull on the dog cheeks to make it angry!</h2>
         <Controls />
       <div className="scene-container">
         <Scene />
